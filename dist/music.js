bd95305842e310e77d05b76fa39b18212412548f
bgyxcarl <yxbg341@gmail.com>
2024-05-22 14:00:38 +0800

    Site updated: 2024-05-22 14:00:38

--- dist/music.js
+++ dist/music.js
@@ -4,16 +4,16 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
-        name: "i miss your warm hands",
-        artist: 'i dont like mirrors',
-        url: 'http://music.163.com/song/media/outer/url?id=2091914322.mp3',
-        cover: 'http://p2.music.126.net/_hL8HIr3wxU8mhAdfqOPxQ==/109951169137349769.jpg?param=300x300',
-      },
-      {
         name: 'A Different Age',
         artist: 'Current Joys',
         url: 'http://music.163.com/song/media/outer/url?id=1324068806.mp3',
         cover: 'http://p2.music.126.net/PY0Gt5OeHOWzbWn6Zp8vgQ==/109951166443166722.jpg?param=300x300',
+      },
+      {
+        name: "i miss your warm hands",
+        artist: 'i dont like mirrors',
+        url: 'http://music.163.com/song/media/outer/url?id=2091914322.mp3',
+        cover: 'http://p2.music.126.net/_hL8HIr3wxU8mhAdfqOPxQ==/109951169137349769.jpg?param=300x300',
       },
       {
         name: 'Everything Reminds Me Of You',
